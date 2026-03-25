0855894b64fd0425d7241085f32890b5e3d08c6c
TheGreatGit <92733551+TheGreatGit@users.noreply.github.com>
2026-03-25 18:00:13 +0000

    Testing chapter draft complete
    wrote up testing rationale and results and put evidence in appendix
    
    Also fixed bug in the TutorProfilePage where, despite the user info being rebuilt after a full browser/app refresh, a logged in user could not then book a lesson.
    
    Fixed the bug by adding a use effect that repoulated the student and tutor ids with relevant varibles as dpendencies.

--- music-tutor-app/frontend/src/pages/TutorProfilePage.jsx
+++ music-tutor-app/frontend/src/pages/TutorProfilePage.jsx
@@ -12,9 +12,9 @@ const TutorProfilePage = () => {
   const { tutorId } = useParams();
 
   // a new function to parse tutorId param to see if it's valid before allowing any fetches based on it
-  const parseTutorId = (id)=>{
+  const parseTutorId = (id) => {
     const parsedId = Number(id);
-    if (!Number.isInteger(parsedId) || parsedId <1) return null;
+    if (!Number.isInteger(parsedId) || parsedId < 1) return null;
     return parsedId;
   };
 
@@ -37,18 +37,32 @@ const TutorProfilePage = () => {
     title: "Lesson",
     teaching_format_id: null, // will be set in the FocusedtutorCard component
     teaching_type_id: null, // will be set in the FocusedtutorCard component
-    skill_level_id: null,// will be set in the FocusedtutorCard component
-    isDraft: true // will be set to false in backend after appointment added to DB
-  })
+    skill_level_id: null, // will be set in the FocusedtutorCard component
+    isDraft: true, // will be set to false in backend after appointment added to DB
+  });
+
+  /* 
+  new useEffect added to repopulate the student id after a full page refresh.
+   although user is rebuilt from the new re-auth code in UserContext, in this page, on refesh, user = null 
+   and its state is not updated even though user is repopulated by re-auth.
+   As such, this useEffect is needed so user is repopulated and lessons can be booked.
+  */
+  useEffect(() => {
+    setDraftBooking((current) => ({
+      ...current,
+      tutor_id: tutorIdParsed,
+      student_id: user?.student_id,
+    }));
+  }, [tutorIdParsed, user]);
 
   // create a parent-component-level state mutator for the draftBooking that child components will use
   // the way this is coded means that children components will not accidentally erase the parts of the draft booking they don't change ( e.g. due to code mistake)
   // i.e. by using ..current, it guarantees that the other properties are kept unchanged and ...current meand only the relevant part is changed in a given mutator ( a button in the FocusedTutorCard component)
   // essemntially, this restricts the change they can make to the minimum necessary
-  const updateDraftBooking = (update) =>{
-    setDraftBooking((current)=> ({...current, ...update}))
+  const updateDraftBooking = (update) => {
+    setDraftBooking((current) => ({ ...current, ...update }));
   };
-  
+
   const [loading, setLoading] = useState(false);
   const [err, setErr] = useState(null); // BREAK THIS IN TO SEPARATE ERRORS FOR THE DIFFERENT USE-EFFECTS? OR 1 SUPER-ERRPR OBJECT THAT HAS 3 PROPETITESFOR THE 3 FETCH ERROR POSSIBILITIES
   const [showCalendar, setShowCalendar] = useState(false);
@@ -56,7 +70,7 @@ const TutorProfilePage = () => {
   // fetch tutor info
   useEffect(() => {
     // prevent pointless fetch if :tutorId param is not a valid number
-    if(!tutorIdParsed) return;
+    if (!tutorIdParsed) return;
 
     const controller = new AbortController();
 
@@ -66,10 +80,13 @@ const TutorProfilePage = () => {
       setErr(null);
 
       try {
-        const res = await fetch(`http://localhost:3000/api/tutors/${tutorIdParsed}`, {
-          credentials: "include",
-          signal: controller.signal,
-        });
+        const res = await fetch(
+          `http://localhost:3000/api/tutors/${tutorIdParsed}`,
+          {
+            credentials: "include",
+            signal: controller.signal,
+          },
+        );
         if (!res.ok) {
           throw new Error("Failed to fetch tutor");
         }
@@ -94,93 +111,103 @@ const TutorProfilePage = () => {
 
   // feth tutor's bookings
   useEffect(() => {
-    if(!tutorIdParsed) return;
+    if (!tutorIdParsed) return;
     const controller = new AbortController();
 
     const getTutorBookings = async () => {
-      setLoading(true)
+      setLoading(true);
       setErr(null);
       try {
-        const res = await fetch(`http://localhost:3000/api/bookings/tutors/${tutorIdParsed}`,{ credentials: "include", signal: controller.signal });
-        if(!res.ok){
+        const res = await fetch(
+          `http://localhost:3000/api/bookings/tutors/${tutorIdParsed}`,
+          { credentials: "include", signal: controller.signal },
+        );
+        if (!res.ok) {
           throw new Error("Failed to fetch tutor's bookings");
         }
         const bookings = await res.json();
         setTutorBookings(bookings);
       } catch (error) {
         if (error.name !== "AbortError") {
-          console.log('error in fetching tutor bookings');
+          console.log("error in fetching tutor bookings");
           setErr(error.message || "Unknown error");
           setTutorBookings([]);
-        }        
+        }
       } finally {
-        setLoading(false)
+        setLoading(false);
       }
     };
     getTutorBookings();
-    return ()=> controller.abort();
+    return () => controller.abort();
   }, [tutorIdParsed]);
-
-
 
   // this is passed doen to Calendar component as a prop
   // it will receive the draftEvent in the scope of the Calendar component where it is called
-  const handleConfirmBooking = async(draftEvent) => {
-    console.log('handleConfirmBooking draftEvent is', draftEvent);
+  const handleConfirmBooking = async (draftEvent) => {
+    console.log("handleConfirmBooking draftEvent is", draftEvent);
     try {
-          const res = await fetch('http://localhost:3000/api/bookings/makeBooking', {method: 'POST', credentials:'include', headers: {
-      "Content-Type": "application/json"
-    }, body: JSON.stringify(
-      {
-        ...draftBooking,
-        // safety check with student id to ensure it's always the most recent one
-        student_id: user?.student_id || null,
-        // safety check with tutor id
-        tutor_id: tutorIdParsed || draftBooking.tutor_id,
-        // use built-in 'toISOstring' method to convert date object to string for sending to DB
-        booking_start_time: draftEvent.booking_start_time.toISOString(),
-        booking_end_time: draftEvent.booking_end_time.toISOString()
-      }
-    )});
+      const res = await fetch(
+        "http://localhost:3000/api/bookings/makeBooking",
+        {
+          method: "POST",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            ...draftBooking,
+            // safety check with student id to ensure it's always the most recent one
+            student_id: user?.student_id || null,
+            // safety check with tutor id
+            tutor_id: tutorIdParsed || draftBooking.tutor_id,
+            // use built-in 'toISOstring' method to convert date object to string for sending to DB
+            booking_start_time: draftEvent.booking_start_time.toISOString(),
+            booking_end_time: draftEvent.booking_end_time.toISOString(),
+          }),
+        },
+      );
 
-    if(!res.ok){
-       throw new Error('Error in booking');
-    }
-    const dbBooking = await res.json();
-    console.log('db booking is', dbBooking);
-    
-    // update the tutor bookings for the calendar in order to update display to include the newly booked appointment
-    // the new booking doesn't need to be altred to have date objects for start and end time here as the bookingShaper() function does that later
-    setTutorBookings((current)=> [...current, dbBooking]);
+      if (!res.ok) {
+        throw new Error("Error in booking");
+      }
+      const dbBooking = await res.json();
+      console.log("db booking is", dbBooking);
+
+      // update the tutor bookings for the calendar in order to update display to include the newly booked appointment
+      // the new booking doesn't need to be altred to have date objects for start and end time here as the bookingShaper() function does that later
+      setTutorBookings((current) => [...current, dbBooking]);
     } catch (error) {
-      console.error('handleConfirmBooking error', error);
+      console.error("handleConfirmBooking error", error);
       // this will be caught by caller in BookingCalendar
       throw error;
     }
-
   };
 
-const handleCancelBooking = async(event) => {
-  if(!event.booking_id) return;
+  const handleCancelBooking = async (event) => {
+    if (!event.booking_id) return;
 
-  const ok = window.confirm('Are you sure you wish to cancel this booking?');
-  if(!ok) return;
+    const ok = window.confirm("Are you sure you wish to cancel this booking?");
+    if (!ok) return;
 
-  try {
-    const res = await fetch(`http://localhost:3000/api/bookings/cancelBooking/${event.booking_id}`, { method: "PATCH", credentials: "include"});
-    // again try new approach where response is json-ed before checking for !res.ok; this way, any backend-specific error message can be obtained
-    const data = await res.json();
+    try {
+      const res = await fetch(
+        `http://localhost:3000/api/bookings/cancelBooking/${event.booking_id}`,
+        { method: "PATCH", credentials: "include" },
+      );
+      // again try new approach where response is json-ed before checking for !res.ok; this way, any backend-specific error message can be obtained
+      const data = await res.json();
 
-    if(!res.ok){
-      throw new Error(data?.message || 'Failed to csncel booking');
+      if (!res.ok) {
+        throw new Error(data?.message || "Failed to csncel booking");
+      }
+
+      // reset tutorBookings to remvoe the cancelled booking-  the useEffect only fires if parsedTutorId changes so need to use setTutorBookings
+      setTutorBookings((current) =>
+        current.filter((booking) => booking.booking_id !== event.booking_id),
+      );
+    } catch (error) {
+      alert(error.message || "Cancellation failed");
     }
-
-    // reset tutorBookings to remvoe the cancelled booking-  the useEffect only fires if parsedTutorId changes so need to use setTutorBookings
-    setTutorBookings((current)=>current.filter((booking)=> booking.booking_id !== event.booking_id ));
-  } catch (error) {
-    alert(error.message || 'Cancellation failed');
-  }
- 
   };
 
   if (loading) return <p className="p-6">Loading tutor...</p>;
@@ -190,18 +217,25 @@ const handleCancelBooking = async(event) => {
   // TRANSFORM TUTOR BOOKINGS INTO EVENT SHAPE THAT THE CALENDAR CAN DISPLAY AND PASS THE TRANSFORMED BOOKINGS AS A PROP
   const transformedBookings = bookingShaper(tutorBookings);
   // create bundled prop for the tutor and calendar child components
-  const draftBookingBundle = {draftBooking, updateDraftBooking}
+  const draftBookingBundle = { draftBooking, updateDraftBooking };
 
   return (
     <div className="min-h-screen bg-slate-50 py-10 px-4">
       <div className="mx-auto max-w-3xl space-y-10">
         <div className="flex flex-col items-center gap-2">
           <div className="w-full max-w-xl">
-            <FocusedTutorCard tutor={tutor} draftBookingBundle={draftBookingBundle} canMessage={ user?.role === 'student'} 
-            onMessageClick={()=>setActiveChat({
-              otherUserId: tutor.user_id,
-              otherDisplayName: `${tutor.first_name} ${tutor.last_name}`.trim()
-            })}/>
+            <FocusedTutorCard
+              tutor={tutor}
+              draftBookingBundle={draftBookingBundle}
+              canMessage={user?.role === "student"}
+              onMessageClick={() =>
+                setActiveChat({
+                  otherUserId: tutor.user_id,
+                  otherDisplayName:
+                    `${tutor.first_name} ${tutor.last_name}`.trim(),
+                })
+              }
+            />
           </div>
 
           <section className="w-full max-w-3xl mx-auto rounded-2xl border border-slate-200 bg-white p-6 shadow-sm">
@@ -209,8 +243,7 @@ const handleCancelBooking = async(event) => {
               Availability and booking
             </h2>
 
-            {
-            user?.role === "student" ? (
+            {user?.role === "student" ? (
               <>
                 <button
                   className="bg-blue-600 hover:bg-blue-700 text-white rounded-lg p-2"
@@ -229,18 +262,18 @@ const handleCancelBooking = async(event) => {
                   <BookingCalendar
                     tutor={tutor}
                     user={user}
-                    calendarEvents = {transformedBookings}
-                    handleConfirmBooking = {handleConfirmBooking}
-                    handleCancelBooking = {handleCancelBooking}
+                    calendarEvents={transformedBookings}
+                    handleConfirmBooking={handleConfirmBooking}
+                    handleCancelBooking={handleCancelBooking}
                     draftBookingBundle={draftBookingBundle}
                   />
                 </div>
               </>
-            ) : user?.role ==='tutor'? (
+            ) : user?.role === "tutor" ? (
               <p className="text-slate-600">
                 Tutors cannot book lessons with other tutors.
               </p>
-            ): (
+            ) : (
               <p>
                 <Link
                   to="/login"
